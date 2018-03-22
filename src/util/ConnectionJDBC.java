146fcee3b0ae74caf193022faae5ed184bb67adc
Cassiano Bortoli <Cassiano Bortoli@192.168.21.140>
2018-03-22 19:39:17 -0300

    Upando pro git

---
+++ src/util/ConnectionJDBC.java
@@ -0,0 +1,53 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class ConnectionJDBC {
+
+    private static Connection connection;
+
+    public static Connection getConnection() throws Exception {
+
+        if (connection == null) {
+            // Carregar o Driver JDBC
+            try {
+                Class.forName("org.firebirdsql.jdbc.FBDriver");
+            } catch (ClassNotFoundException ex) {
+                throw new Exception(ex);
+            }
+
+            /**
+             * Conectar ao Banco de Dados URL:
+             * jdbc:nomeDoProduto:servidor:porta:nomeDoBanco
+             */
+            String ip = "localhost";
+            String database = "E:\\OneDrive\\Documentos\\AULA\\JAVA\\DB JAVA\\AULA06.FDB";
+            String url = "jdbc:firebirdsql:" + ip + "/3050:" + database;
+            String usuario = "SYSDBA";
+            String senha = "masterkey";
+
+            try {
+                // Obter uma conexao
+                connection = DriverManager.getConnection(url, usuario, senha);
+            } catch (SQLException ex) {
+                throw new Exception(ex);
+            }
+        }
+
+        return connection;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Teste!");
+        try {
+            ConnectionJDBC.getConnection();
+        } catch (Exception ex) {
+            System.out.println(""+ex.getMessage());
+        }
+    }
+}
